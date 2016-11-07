10dc6ee69a24474916d5ff9c83142404c00f62a8
RyomaK <ryooomaaa0413@outlook.com>
2016-11-07 19:34:44 +0900

    second commit

---
+++ app.rb
@@ -0,0 +1,56 @@
+	require "sinatra"
+	require 'active_record'
+	require 'sinatra/reloader'
+	register Sinatra::Reloader
+	ActiveRecord::Base.establish_connection(
+		"adapter" => "sqlite3",
+		"database" => "./score.db"
+		)
+	class Score < ActiveRecord::Base
+	end
+
+	get "/" do 
+		erb:index
+	end
+
+	get "/score_write" do
+		erb:score_write
+	end
+
+	get "/score" do
+		@scores = Score.order("id desc").all
+		erb:score
+	end
+
+	post "/new" do 
+		Score.create({  :id => params[:id],
+						:school_name => params[:school_name],
+		 				:semester => params[:semester],
+						:japanese => params[:japanese],
+						:english => params[:english],
+						:math => params[:math],
+						:science => params[:science],
+						:social => params[:social],
+						:test => params[:test]
+						})
+		redirect '/score_write'    
+		erb :score_write
+	end
+
+
+	post "/check" do
+		@semester = params[:semester]
+		@test = params[:test]
+		@school_name = params[:school_name]
+		@japanese =  params[:japanese]
+		@math =  params[:math]
+		@english =  params[:english]
+		@science =  params[:science]
+		@social =  params[:social]
+		erb:check
+	end
+
+	get "/score_write" do
+		erb:score
+	end
+	
---
+++ import.sql
@@ -0,0 +1,11 @@
+create table scores(
+    id integer primary key,
+    test text,
+    school_name text,
+    semester integer,
+    japanese integer,
+    math integer,
+    english integer,
+    science integer,
+    social integer
+);
